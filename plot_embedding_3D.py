5f3d8562240176616ce506848210ef68caa2a569
Luann Jung <lollyluann@gmail.com>
2018-06-18 21:22:50 +0000

    new plot

--- plot_embedding_3D.py
+++ plot_embedding_3D.py
@@ -2,9 +2,11 @@ from mpl_toolkits.mplot3d import Axes3D
 from sklearn.decomposition import PCA
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
+from numpy import loadtxt
 import codecs
 import numpy
 import sys
+import ast
 import os
 
 # First argument is the vector file as a .txt
@@ -25,12 +27,36 @@ def build_word_vector_matrix(vector_file, n_words):
 
 # MAIN PROGRAM
 vector_file = sys.argv[1]
+clusters = sys.argv[2]
 path_vectors, paths = build_word_vector_matrix(vector_file, 14869)
 
 # project the vectors down into R^3. 
 pca = PCA(n_components=3)
 reduced_path_vectors = pca.fit_transform(path_vectors)
 
+# create a dict that maps paths to 3D vectors
+reduced_path_dict = {}
+for i in range(len(paths)):
+    reduced_path_dict.update({paths[i]:reduced_path_vectors[i]})
+
+#print(reduced_path_dict)
+
+# plot in 3D
+fig = plt.figure(figsize=(20,10))
+ax = Axes3D(fig)
+
+with open(clusters) as f:
+    cluster_list = f.readlines()
+# list of clusters, each cluster is a list of paths as strings
+for cluster in cluster_list:
+    color = numpy.random.rand(3,)
+    cluster_path_list = ast.literal_eval(cluster)
+    for path_name in cluster_path_list:
+        vector = reduced_path_dict.get(path_name)    
+        ax.scatter(vector[0], vector[1], vector[2], s=fig.dpi/72., marker='.',c=color)
+       
+plt.savefig("embedding_3D", dpi=1000)
+'''
 # get list of x,y,z components
 x = []
 y = []
@@ -44,7 +70,6 @@ for vector in reduced_path_vectors:
 # plot in 3D
 fig = plt.figure(figsize=(20,10))
 ax = Axes3D(fig)
-#ax = fig.add_subplot(111, projection='3d')
 ax.scatter(x, y, z, s=fig.dpi/72., marker='.')
 plt.savefig("embedding_3D", dpi=1000)
-
+'''
