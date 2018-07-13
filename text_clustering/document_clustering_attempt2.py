18643957a7f19727d4eddb43a439fb0643744dbe
Luann Jung <lollyluann@gmail.com>
2018-07-13 04:29:07 +0000

    pLESSSS WORK

--- text_clustering/document_clustering_attempt2.py
+++ text_clustering/document_clustering_attempt2.py
@@ -349,7 +349,7 @@ def bar_clusters(frame, num_clusters, home_dir):
         plt.xlabel('Directory')
         plt.title('Directories in Cluster ' + str(i) + "\n" + cluster_stats)
         plt.tight_layout()
-        plt.subplots_adjust(bottom=0.17, top=0.75)
+        plt.subplots_adjust(bottom=0.3, top=0.6)
         save_name = "barchart_cluster"+str(i)       
         # plt.savefig(save_name, dpi=200)
         
